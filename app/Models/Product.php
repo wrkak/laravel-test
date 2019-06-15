39ae49eab45f3fea80e8747708bc825d33db2cf2
k <710312372@qq.com>
2019-06-15 09:47:44 +0000

    初始化众筹模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+     const TYPE_NORMAL = 'normal';
+     const TYPE_CROWDFUNDING = 'crowdfunding';
+
+    public static $typeMap = [
+        self::TYPE_NORMAL  => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
+
      protected $fillable = [
         'title',
         'description',
@@ -16,6 +24,7 @@ class Product extends Model
         'sold_count',
         'review_count',
         'price',
+        'type',
     ];
 
     protected $casts = [
@@ -41,4 +50,8 @@ class Product extends Model
         return $this->belongsTo(Category::class);
     }
 
+    public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
+    }
 }
