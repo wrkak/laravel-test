c7a50f7c9243c352087f603f29d17b76289ded49
k <710312372@qq.com>
2019-06-17 10:23:24 +0000

    众筹商品详情页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CrowdFundingOrderRequest;
+use App\Models\ProductSku;
 use App\Http\Requests\OrderRequest;
 use App\Exceptions\CouponCodeUnavailableException;
 use App\Models\CouponCode;
@@ -201,4 +203,14 @@ class OrdersController extends Controller
         return $order;
     }
 
+        // 创建一个新的方法用于接受众筹商品下单请求
+    public function crowdfunding(CrowdFundingOrderRequest $request, OrderService $orderService)
+    {
+        $user    = $request->user();
+        $sku     = ProductSku::find($request->input('sku_id'));
+        $address = UserAddress::find($request->input('address_id'));
+        $amount  = $request->input('amount');
+
+        return $orderService->crowdfunding($user, $address, $sku, $amount);
+    }
 }
--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -68,20 +68,7 @@ class PaymentController extends Controller
         return app('alipay')->success();
     }
 
-    public function payByWechat(Order $order, Request $request) {
-        // 校验权限
-    	$this->authorize('own', $order);
-        // 校验订单状态
-    	if ($order->paid_at || $order->closed) {
-    		throw new InvalidRequestException('订单状态不正确');
-    	}
-        // scan 方法为拉起微信扫码支付
-    	return app('wechat_pay')->scan([
-            'out_trade_no' => $order->no,  // 商户订单流水号，与支付宝 out_trade_no 一样
-            'total_fee' => $order->total_amount * 100, // 与支付宝不同，微信支付的金额单位是分。
-            'body'      => '支付 Laravel Shop 的订单：'.$order->no, // 订单描述
-        ]);
-    }
+
 
     public function wechatNotify()
     {
