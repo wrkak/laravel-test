3e24c2c039a001275a23c2a40580a9bdfbc75b2f
k <710312372@qq.com>
2019-06-26 07:50:25 +0000

    用户注册接口

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use App\Models\Image;
+use App\Http\Requests\Api\UserRequest;
+
+class UsersController extends Controller
+{
+	public function store(UserRequest $request)
+    {
+
+        $verifyData = \Cache::get($request->verification_key);
+        if (!$verifyData) {
+            return $this->response->error('验证码已失效', 422);
+        }
+        if (!hash_equals($verifyData['code'], $request->verification_code)) {
+            return $this->response->error('验证码错误', 401);
+        }
+        $user = User::create([
+            'name' => $request->name,
+            'phone' => $verifyData['phone'],
+            'password' => bcrypt($request->password),
+        ]);
+
+        // 清除验证码缓存
+        \Cache::forget($request->verification_key);
+
+         // return $this->response->item($user, new UserTransformer())
+         //    ->setMeta([
+         //        'access_token' => \Auth::guard('api')->fromUser($user),
+         //        'token_type' => 'Bearer',
+         //        'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+         //    ])
+         //    ->setStatusCode(201);
+        return $this->response->created();
+    }
+}
