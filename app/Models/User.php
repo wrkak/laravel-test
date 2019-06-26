d4639cd32083994a6e2a2d817df34ff966c2e052
k <710312372@qq.com>
2019-06-26 10:46:50 +0000

    微信登录功能开发

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','email_verified','phone'
+        'name', 'email', 'password','email_verified','phone', 'weixin_openid', 'weixin_unionid',
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -39,6 +39,10 @@ $api->version('v1', [
 		$api->post('captchas', 'CaptchasController@store')
 			->name('api.captchas.store');
 
+        // 第三方登录
+        $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+            ->name('api.socials.authorizations.store');
+
 	});
 
 
