9e43e29cd58d283625b16a647f5531f0970c9d8f
k <710312372@qq.com>
2019-06-27 10:58:30 +0000

    api/user   Fractal，Transformer，dingo-serializer-switch

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
-use App\Models\Image;
+use App\Transformers\UserTransformer;
 use App\Http\Requests\Api\UserRequest;
 
 class UsersController extends Controller
@@ -27,13 +27,19 @@ class UsersController extends Controller
         // 清除验证码缓存
         \Cache::forget($request->verification_key);
 
-         // return $this->response->item($user, new UserTransformer())
-         //    ->setMeta([
-         //        'access_token' => \Auth::guard('api')->fromUser($user),
-         //        'token_type' => 'Bearer',
-         //        'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
-         //    ])
-         //    ->setStatusCode(201);
-        return $this->response->created();
+         return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])
+            ->setStatusCode(201);
+        // return $this->response->created();
     }
+
+    public function me()
+    {	
+        return $this->response->item($this->user(), new UserTransformer());
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,8 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-	'namespace' => 'App\Http\Controllers\Api'
+	'namespace' => 'App\Http\Controllers\Api',
+	'middleware' => 'serializer:array'
 ], function($api) {
 
 
@@ -51,12 +52,91 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
-            
+
 
 	});
 
 
+   $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) {
+        // 游客可以访问的接口
+        // $api->get('categories', 'CategoriesController@index')
+        //     ->name('api.categories.index');
+        //     // 话题列表
+        // $api->get('topics', 'TopicsController@index')
+        //     ->name('api.topics.index');
+        //    // 某个用户发布的话题
+        //  $api->get('users/{user}/topics', 'TopicsController@userIndex')
+        //     ->name('api.users.topics.index');
+        //    // 话题详情
+        // $api->get('topics/{topic}', 'TopicsController@show')
+        //     ->name('api.topics.show');
+        // // 话题回复列表
+        // $api->get('topics/{topic}/replies', 'RepliesController@index')
+        //     ->name('api.topics.replies.index');
+        // // 某个用户的回复列表
+        // $api->get('users/{user}/replies', 'RepliesController@userIndex')
+        //     ->name('api.users.replies.index');
+        //         // 资源推荐
+        // $api->get('links', 'LinksController@index')
+        //     ->name('api.links.index');
+        // // 活跃用户
+        // $api->get('actived/users', 'UsersController@activedIndex')
+        //     ->name('api.actived.users.index');
 
+
+
+
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+            //  // 编辑登录用户信息
+            // $api->patch('user', 'UsersController@update')
+            //     ->name('api.user.update');
+            // $api->put('user', 'UsersController@update')
+            //     ->name('api.user.update');
+
+            // // 图片资源
+            // $api->post('images', 'ImagesController@store')
+            //     ->name('api.images.store');
+
+            // // 发布话题
+            // $api->post('topics', 'TopicsController@store')
+            //     ->name('api.topics.store');
+            // $api->patch('topics/{topic}', 'TopicsController@update')
+            //     ->name('api.topics.update');
+            // $api->delete('topics/{topic}', 'TopicsController@destroy')
+            //     ->name('api.topics.destroy');
+
+            // // 发布回复
+            // $api->post('topics/{topic}/replies', 'RepliesController@store')
+            //     ->name('api.topics.replies.store');
+            // $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')
+            //     ->name('api.topics.replies.destroy');
+
+            //  // 通知列表
+            // $api->get('user/notifications', 'NotificationsController@index')
+            //     ->name('api.user.notifications.index');
+            // // 通知统计
+            // $api->get('user/notifications/stats', 'NotificationsController@stats')
+            //     ->name('api.user.notifications.stats');
+            // // 标记消息通知为已读
+            // $api->patch('user/read/notifications', 'NotificationsController@read')
+            //     ->name('api.user.notifications.read');
+            // $api->put('user/read/notifications', 'NotificationsController@read')
+            //     ->name('api.user.notifications.read.put');
+
+            // // 当前登录用户权限
+            // $api->get('user/permissions', 'PermissionsController@index')
+            //     ->name('api.user.permissions.index');
+        });
+
+    });
 
 });
 
